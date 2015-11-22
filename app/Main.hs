a6157f67ed6b4cd4398f9227ecd4987ee69d01f3
Daniel Gazard <daniel.gazard@gmail.com>
2015-11-22 22:59:08 +0100

    Separate simulation and pricing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Montecarlo
+import System.Random
+import Data.Monoid.Average
 
 call :: Double -> Double -> Double
 call strike s = max (s - strike) 0
@@ -8,6 +10,20 @@ call strike s = max (s - strike) 0
 samplesNum :: Int
 samplesNum = 1000000
 
+randomMc :: Montecarlo StdGen Double
+randomMc = liftMontecarlo random
+
+mc :: Montecarlo StdGen Double
+mc = do
+  x <- randomMc
+  return $ x * 3
+
+avg :: Fractional a => [a] -> a
+avg xs = sum xs / realToFrac (length xs)
+
 main :: IO ()
-main =
-  print $ montecarlo 100 0.05 0.3 0.25 (call 100) samplesNum
+main = do
+--  print $ montecarlo 100 0.05 0.3 0.25 (call 100) samplesNum
+  print $ evalMontecarlo 10 mc avg (mkStdGen 42)
+  print $ evalMontecarloPar 1000 100 mc avg (mkStdGen 42)
+
--- src/Montecarlo.hs
+++ src/Montecarlo.hs
@@ -1,10 +1,17 @@
 module Montecarlo
     (
-     montecarlo
+      montecarlo
+    , Montecarlo
+    , evalMontecarlo
+    , evalMontecarloPar
+    , liftMontecarlo
     ) where
 
 import Data.Random.Normal
 import Control.Parallel.Strategies
+import System.Random
+import Control.Monad.State
+import Control.Monad
 
 montecarlo' :: Double -> Double -> Double ->
                Double -> (Double -> Double) -> Int -> Int -> Double
@@ -23,7 +30,31 @@ montecarlo s rate vol time_to_expiry payoff n = do
   let chunkSize = 1000
       chunksCount = n `div` chunkSize
       seeds = take chunksCount [1..]
-      payoffs = map (montecarlo' s rate vol time_to_expiry payoff chunkSize) seeds
-                `using` parList rdeepseq
+      payoffs = map (montecarlo' s rate vol time_to_expiry payoff chunkSize)
+                seeds `using` parList rdeepseq
   sum payoffs / realToFrac chunksCount
+
+{--
+toto :: Double
+toto = fst $ normal (mkStdGen 42)
+
+foo :: State StdGen Double
+foo = do
+  g <- get
+  state $ random
+--}
+
+type Montecarlo g a = State g a
+
+evalMontecarlo :: RandomGen g => Int -> Montecarlo g a -> ([a] -> b) -> g -> b
+evalMontecarlo n mc reducer g = reducer $ evalState (replicateM n mc) g
+
+evalMontecarloPar :: RandomGen g => Int -> Int -> Montecarlo g a ->
+                     ([a] -> a) -> g -> a
+evalMontecarloPar n c mc reducer g = reducer mcs
+    where gs = take n (map snd (iterate (next . snd) (0, g)))
+          mcs = map (evalMontecarlo c mc reducer) gs `using` parList rseq
+
+liftMontecarlo :: RandomGen g => (g -> (a, g)) -> Montecarlo g a
+liftMontecarlo = state
 
