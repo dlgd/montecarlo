f425fd86437e7ccf83f9b1edeb469415b31f92aa
Daniel Gazard <daniel.gazard@gmail.com>
2015-12-12 23:08:16 +0100

    Add montecarlo unit test that computes pi

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+import Montecarlo
+import System.Random
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+mcPi :: Montecarlo StdGen [Double]
+mcPi =  dist <$> mkValue <*> mkValue
+    where mkValue = liftMontecarlo $ randomR (0, 1)
+          dist x y = [sqrt $ x**2 + y**2]
+
+evalPi :: Double
+evalPi = 4 * fromIntegral (length (filter (<=1) xs)) / fromIntegral (length xs)
+    where xs = evalMontecarlo 100000 mcPi (mkStdGen 42)
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests" [
+             testCase "montecarlo: pi computation" $
+             assertBool "" (abs (evalPi - pi) < 0.00001)
+            ]
